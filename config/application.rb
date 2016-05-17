6e96809174c8a61b16f12eff4d46525aeb84c55c
sanakm <skass094@gmail.com>
2016-05-17 23:33:22 +0000

    new front page

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,16 @@ module Relocate
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << "#{Rails.root}/app/assets/videos"
+    # Disable Rails's static asset server (Apache or nginx will already do this)  
+    config.serve_static_files = true
+
+    # Compress JavaScripts and CSS  
+    config.assets.compress = true
+
+    # Don't fallback to assets pipeline if a precompiled asset is missed  
+    config.assets.compile = true
+
+    # Generate digests for assets URLs  
+    config.assets.digest = true
   end
 end
