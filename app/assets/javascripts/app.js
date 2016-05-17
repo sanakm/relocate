f19207179f1dca3ff14bd717b5f6b283b873bf20
Dave <emaloveshoe@gmail.com>
2016-05-17 19:12:14 -0400

    init

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -56,6 +56,8 @@ $(function() {
     }
   }
 
+  var cities = []; // global
+
   $('#submit_profession').on('click', function(e) {
     e.preventDefault();
     var searchTerm = $("#tags").val();
@@ -68,11 +70,60 @@ $(function() {
       url: "/cities",
       type: "GET"
     }).done(function(data) {
-      data.forEach(function(city) {
-        $("#results").append("<p>" + city.name + "</p>");
-      });
+        cities = data; // set cities as global
+        if (searchTerm === "Web Developer") {
+            var sorted = data.sort(function(a,b){
+                var a1=a.webdev_rating, b1=b.webdev_rating;
+                if(a1==b1) return 0;
+                return a1 < b1 ? 1: -1;
+            });
+            sorted.forEach(function(city) {
+                console.log("city");
+              $("#results").append("<p class='dev_city' data-city-id='" + city.id + "'>" + city.name + " dev_rating " + city.webdev_rating + "</p>");
+            
+                            
+            });
+
+        
+        } else if(searchTerm === "Accounting") {
+            var sorted = data.sort(function(a,b){
+                var a1=a.accountant_rating, b1=b.accountant_rating;
+                if(a1==b1) return 0;
+                return a1 < b1 ? 1: -1;
+            });
+            sorted.forEach(function(city) {
+              $("#results").append("<p id='accountant_city'>" + city.name + " accountant_rating " + city.accountant_rating + "</p>");
+            });
+
+        } else if(searchTerm === "Graphic Designer") {
+            var sorted = data.sort(function(a,b){
+                var a1=a.graphic_designer_rating, b1=b.graphic_designer_rating;
+                if(a1==b1) return 0;
+                return a1 < b1 ? 1: -1;
+            });
+            sorted.forEach(function(city) {
+              $("#results").append("<p id='designer_city'>" + city.name + " graphic_designer_rating " + city.graphic_designer_rating + "</p>");
+            });
+        
+        } else {
+            $("#results").append("<p>" + "Acessing database..." + "</p>").addClass("loadinggif");
+        }
     });
 
   });
-  
+
+    $('#results').on('click', '.dev_city', function(e) {
+        var current_city_id = $(this).data("city-id");
+        cities.forEach(function(city) {
+            if (city.id == current_city_id) {
+                selected_city = city;
+            }
+        });
+        var new_results = $(".city_details");
+        new_results.html("");
+        $(".city_details").append("<p>City Name:" + selected_city.name + "<br><br></p>");
+        $(".city_details").append("<p>City Country:" + selected_city.country + "</p><br><br>");
+        $(".city_details").append("<p>Country General Info:" + selected_city.general_info1 + "</p>");
+
+    });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513090837) do
+ActiveRecord::Schema.define(version: 20160517160659) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20160513090837) do
     t.integer  "journalist_avg_salary"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.integer  "webdev_rating"
+    t.integer  "webdev_avg_salary"
+    t.string   "gps_coordinates"
+    t.integer  "web_dev_shortage"
   end
 
 end
