75cde542e07b2945537072772099ae94239ea210
sanakm <skass094@gmail.com>
2016-05-19 21:57:53 +0000

    Fixing broken schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160519202121) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
